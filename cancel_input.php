20be8ed06b50904d110641aba57ddcb13be1d901
Tonmoy-Saha1 <tsaha182014@bscse.uiu.ac.bd>
2022-03-20 23:13:14 +0600

    cancel_input.php commited

---
+++ cancel_input.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="cancel.php" method="Post">
+        <p>
+            <label for="Number">Number: </label>
+            <input type="text" name="number">
+        </p>
+        <p>
+            <label for="Doctor">Doctor: </label>
+            <input type="text" name="doctor">
+        </p>
+        <button>Submit</button>
+    </form>
+</body>
+</html>
