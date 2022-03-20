39df0965d2ad329bf86f0d8fc80b54c2506ed6c7
Tonmoy-Saha1 <tsaha182014@bscse.uiu.ac.bd>
2022-03-20 22:39:16 +0600

    commit for appt_input.php

---
+++ appt_input.php
@@ -0,0 +1,37 @@
+<?php
+    include("db_config.php");
+
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>App-Fontend</title>
+    </head>
+    <body>
+        <form action="appt.php" method="POST">
+            <p>
+                <label for="Name">Name: </label>
+                <input type="text" name="name>
+            </p>
+            <br>
+            <p>
+                <label for="Number">Number: </label>
+                <input type="text" name="number">
+            </p>
+            <p>
+                <label for="Date">Date: </label>
+                <input type="text" name="Date">
+            </p>
+            <p>
+                <label for="Doctor name">Doctor name: </label>
+                <input type="text" name="dname">
+            </p>
+            <button>Submit</button>
+        </form>
+    </body>
+    </html>
+    <?php
+?>
